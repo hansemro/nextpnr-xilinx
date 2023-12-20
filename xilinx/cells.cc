a8709c9be95265054757c23b3f8ac276d17dae6e
Hansem Ro <hansemro@outlook.com>
2023-12-19 16:00:56 -0800

    xilinx: specify RAMS32 and RAMS64E in cells.cc, constids.inc, json2dcp.java
    Signed-off-by: Hansem Ro <hansemro@outlook.com>

--- xilinx/cells.cc
+++ xilinx/cells.cc
@@ -41,11 +41,27 @@ std::unique_ptr<CellInfo> create_cell(Context *ctx, IdString type, IdString name
         add_port("I", PORT_IN);
         add_port("WE", PORT_IN);
         add_port("O", PORT_OUT);
+    } else if (type == ctx->id("RAMS64E")) {
+        for (int i = 0; i < 6; i++)
+            add_port("ADR" + std::to_string(i), PORT_IN);
+        for (int i = 6; i < 8; i++)
+            add_port("WADR" + std::to_string(i), PORT_IN);
+        add_port("CLK", PORT_IN);
+        add_port("I", PORT_IN);
+        add_port("WE", PORT_IN);
+        add_port("O", PORT_OUT);
     } else if (type == ctx->id("RAMD32")) {
         for (int i = 0; i < 5; i++)
             add_port("RADR" + std::to_string(i), PORT_IN);
         for (int i = 0; i < 5; i++)
             add_port("WADR" + std::to_string(i), PORT_IN);
+        add_port("CLK", PORT_IN);
+        add_port("I", PORT_IN);
+        add_port("WE", PORT_IN);
+        add_port("O", PORT_OUT);
+    } else if (type == ctx->id("RAMS32")) {
+        for (int i = 0; i < 5; i++)
+            add_port("ADR" + std::to_string(i), PORT_IN);
         add_port("CLK", PORT_IN);
         add_port("I", PORT_IN);
         add_port("WE", PORT_IN);
