7f459c9a18ee109229377c97fd95891b5a696f6c
Hansem Ro <hansemro@outlook.com>
2024-06-22 21:47:35 -0700

    xilinx: reverse RAM32M INIT string generation

--- xilinx/pack_dram.cc
+++ xilinx/pack_dram.cc
@@ -468,7 +468,7 @@ void XilinxPacker::pack_dram()
                             auto orig_init =
                                     ci->params.at(ctx->id(stringf("INIT_%c", 'A' + i))).extract(0, 64).as_bits();
                             std::string init;
-                            for (int k = 0; k < 32; k++) {
+                            for (int k = 31; k >= 0; k--) {
                                 init.push_back(orig_init.at(k * 2 + j) ? Property::State::S1 : Property::State::S0);
                             }
                             dram->params[ctx->id("INIT")] = Property::from_string(init);
